b55b4ab2a9941c961d9a0b5f4919369141a4830d
JC <64677547+JamesCicada@users.noreply.github.com>
2024-03-19 10:11:22 +0000

    Small updates

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -20,7 +20,7 @@ const SearchBar = () => {
     }
 
     return (
-        <div className="relative w-full h-14 flex flex-col bg-white dark:bg-slate-800">
+        <div className="relative w-full h-14 flex flex-col bg-white dark:bg-slate-800 rounded-t-md">
             <div className="relative h-14 z-10 rounded-md">
                 <Input disabled={isSearching} value={query} onChange={(e) => setQuery(e.target.value)} onKeyDown={(e) => {
                     if(e.key === "Enter"){
@@ -29,9 +29,9 @@ const SearchBar = () => {
                     if(e.key === "Escape"){
                         inputRef?.current?.blur()
                     }
-                }} ref={inputRef} className="absolute inset-0 h-full " />
-                <Button disabled={isSearching} onClick={search} className="sm absolute right-0 h-full inset-y-0 rounded-l-none">
-                    {isSearching ? <Loader2 className="w-6 h-6 animate-spin" /> : <SearchIcon className="w-6 h-6" />}
+                }} ref={inputRef} className="absolute inset-0 h-full rounded-md dark:bg-slate-800" placeholder="Search" />
+                <Button disabled={isSearching} onClick={search} className="sm absolute right-0 h-full inset-y-0 rounded-l-none bg-white dark:bg-slate-800">
+                    {isSearching ? <Loader2 className="w-6 h-6 animate-spin text-zinc-500 dark:text-slate-300" /> : <SearchIcon className="w-6 h-6 text-gray-500 dark:text-slate-300" />}
                 </Button>
             </div>
         </div>
